cbb69bbe242e5e3e2f9b0a3ecb0f7efac8c74918
mentavishal1024 <mentavishal4201@gmail.com>
2024-09-25 08:08:24 +0000

    congratulations-card

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="background">
+    <h1>Congratulations</h1>
+    <div className="card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran v</h1>
+      <p>Vishnu institute of Computer Education and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
